e122699a8a6144587a17b4b2621633fc5d9b049f
nicolas <nicolas110996@example.com>
2021-02-14 15:51:15 -0300

    Create project

---
+++ src/main/java/ftz/FtzApplication.java
@@ -0,0 +1,13 @@
+package ftz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ftz/FtzApplicationTests.java
@@ -0,0 +1,13 @@
+package ftz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
