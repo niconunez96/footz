04a6a2dbf00b63f83e6113795fb877704f6c721a
nicolas <nicolas110996@example.com>
2021-03-17 21:24:18 -0300

    Add findAll method to jpaRepository

--- src/main/java/shared/infrastructure/jpa/JPARepository.java
+++ src/main/java/shared/infrastructure/jpa/JPARepository.java
@@ -7,17 +7,28 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
 import java.util.Optional;
 
 public abstract class JPARepository<T, ID> {
 
     @Autowired
-    private EntityManagerFactory entityManagerFactory;
+    protected EntityManagerFactory entityManagerFactory;
 
     protected Optional<T> findById(ID id){
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         T entity = entityManager.find(getEntityClass(), id);
         return Optional.ofNullable(entity);
+    }
+
+    protected List<T> findAll(){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        CriteriaBuilder criteria = entityManager.getCriteriaBuilder();
+        CriteriaQuery<T> query = criteria.createQuery(getEntityClass());
+        query.from(getEntityClass());
+        return entityManager.createQuery(query).getResultList();
     }
 
     @Transactional
